b53ce9ca86872164770e2673e6fbdd1e8843a761
ro-jun <thdghwns1191@naver.com>
2024-12-10 03:02:24 +0900

    feat: implement filtering logic for dataset preprocessing

---
+++ data_filtering.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import json
+import os
+
+# Chrome 브라우저 자동 관리 및 실행
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-gpu")  # GPU 가속 비활성화
+options.add_argument("--no-sandbox")  # 샌드박스 모드 비활성화(권한 문제 방지)
+
+# ChromeDriver 없이 실행
+driver = webdriver.Chrome(options=options)  
+
+# Kaggle 필터 적용된 URL로 이동 (csv, 24-12-09 당시 Highly-voted: LLM Fine-Tuning)
+# url = "https://www.kaggle.com/datasets?fileType=csv&feedbackIds=16"
+# Kaggle 필터 적용된 URL로 이동 (csv, 24-12-09 당시 Highly-voted: Research)
+# url = "https://www.kaggle.com/datasets?fileType=csv&feedbackIds=9"
+# Kaggle 필터 적용된 URL로 이동 (csv, 24-12-09 당시 Highly-voted: Well-documented)
+url = "https://www.kaggle.com/datasets?fileType=csv&feedbackIds=11"
+
+driver.get(url)
+
+# 페이지 로드 대기 (JavaScript 렌더링을 위해)
+time.sleep(3)
+
+datasets = []
+
+while True:
+    # 데이터셋 카드 요소 찾기
+    dataset_cards = driver.find_elements(By.XPATH, '//div[contains(@class, "sc-kqLymz")]/ul[contains(@class, "MuiList-root km-list css-1uzmcsd")]/li/div')
+
+    for card in dataset_cards:
+        try:
+            # 제목, 파일타입, 링크 추출
+            title = card.find_element(By.CSS_SELECTOR, 'a').get_attribute("aria-label") # 제목
+            file_type = card.find_element(By.XPATH, './/span[2][contains(@class, "sc-geXuza")]').text  # 유용성, 파일타입, 파일크기
+            link = card.find_element(By.CSS_SELECTOR, 'a').get_attribute("href")  # 링크(url)
+            datasets.append({
+                "title": title,
+                "file_type":file_type,
+                "url": link,
+            })
+        except Exception as e:
+            print(f"오류 발생: {e}")
+
+    # 다음 페이지 버튼 찾기
+    try:
+        next_button = driver.find_element(By.XPATH, '//button[contains(@aria-label, "Go to next page")]')
+        # 버튼이 비활성화된 경우 종료
+        if "disabled" in next_button.get_attribute("class"):
+            print("더 이상 페이지가 없습니다. 종료합니다.")
+            break
+
+        # 다음 페이지 버튼 클릭
+        driver.execute_script("arguments[0].click();", next_button)
+        time.sleep(3)  # 페이지 로드 대기
+    except Exception as e:
+        print(f"다음 페이지 버튼을 찾을 수 없습니다: {e}")
+        break  # 버튼이 없을 경우 종료
+
+# 브라우저 종료
+driver.quit()
+
+# 저장 경로와 파일명 설정
+output_folder = "D:/kaggle/Kaggle_crawling/kaggle_datasets/filtered_datasets"
+output_file = os.path.join(output_folder, "Well-documented.json")  # 특수 문자 제거
+
+# JSON 파일로 저장
+with open(output_file, "w", encoding="utf-8") as f:
+    json.dump(datasets, f, ensure_ascii=False, indent=4)
+
+print(f"데이터가 {output_file}에 저장되었습니다.")
+
+# 결과 출력
+print("데이터셋 목록:")
+for dataset in datasets:
+    print(dataset)
+print(f"데이터 추출 갯수: {len(datasets)}")
